93d8c3de103e55494c9025decfb5cc5b01eec03d
John Fulton <fulton@redhat.com>
2021-06-16 23:03:55 -0400

    standard undeploy should remove deployed-network file

--- standard/undeploy.sh
+++ standard/undeploy.sh
@@ -11,10 +11,10 @@ openstack overcloud delete $STACK --yes
 
 pushd ../metalsmith
 bash unprovision.sh $STACK
-rm -f deployed-metal-$STACK.yaml
+rm -f deployed-{metal,network}-$STACK.yaml
 popd
 
-for F in deployed-metal-$STACK.yaml cirros-0.4.0-x86_64-disk.{raw,img} tempest-deployer-input.conf; do
+for F in deployed-{metal,network}-$STACK.yaml cirros-0.4.0-x86_64-disk.{raw,img} tempest-deployer-input.conf; do
     if [[ -e $F ]]; then
         rm -f $F
     fi
