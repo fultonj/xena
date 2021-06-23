967dd11727fc68551666bcc09a8fba26c6dae93c
John Fulton <fulton@redhat.com>
2021-06-23 04:04:30 +0000

    POC of deployed_ceph is working
    A run of this ceph.sh with latest patches [1] looks like this:
    
    http://paste.openstack.org/show/806875
    
    [1] https://review.opendev.org/c/openstack/tripleo-ansible/+/797538/3

--- deployed_ceph/ceph.sh
+++ deployed_ceph/ceph.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 
+USER=0
+
 # GET AN INVENTORY
+STACK=other-3
+WORKING_DIR="$HOME/overcloud-deploy/${STACK}"
+INV="$WORKING_DIR/tripleo-ansible-inventory.yaml"
+# This inventory is a result of baremetal provisioning, see:
+#   tripleo_ansible/playbooks/cli-overcloud-node-provision.yaml
+ROLES="/usr/share/openstack-tripleo-heat-templates/roles_data.yaml"
+PLAYBOOKS="$HOME/tripleo-ansible/tripleo_ansible/playbooks"
 
-# PLAY: undercloud,overcloud
 # CREATE CEPHADM USER
-# - name: Prepare cephadm user and keys
-#   include_role:
-#     name: tripleo_run_cephadm
-#     tasks_from: enable_ceph_admin_user.yml
-#
-# https://github.com/openstack/tripleo-ansible/blob/master/tripleo_ansible/roles/tripleo_run_cephadm/tasks/enable_ceph_admin_user.yml
-#
-# https://github.com/openstack/tripleo-ansible/blob/master/tripleo_ansible/playbooks/ceph-admin-user-playbook.yml
+if [[ USER -eq 1 ]]; then
+    # We will want to map composed $ROLES to groups based on services
+    # For now we assume defaults
+    #CEPHADM_PUBLIC_PRIVATE_SSH_LIST="undercloud,ceph_mon,ceph_mgr"
+    CEPHADM_PUBLIC_PRIVATE_SSH_LIST="undercloud,Controller"
+    #CEPHADM_PUBLIC_SSH_LIST="undercloud,ceph_osd,ceph_rgw,ceph_mds,ceph_nfs,ceph_rbdmirror"
+    CEPHADM_PUBLIC_SSH_LIST="undercloud,CephStorage"
 
-# PLAY: localhost
-# CREATE SPEC
-# - name: genereate ceph_spec for bootstrap
-#   ceph_spec_bootstrap:
-#     new_ceph_spec: "{{ tripleo_run_cephadm_spec_path }}"
-#     tripleo_ansible_inventory: "{{ inventory_file }}"
-#     fqdn: "{{ ceph_spec_fqdn }}"
-#     osd_spec: "{{ ceph_osd_spec }}"
+    ansible-playbook -i $INV \
+                     $PLAYBOOKS/ceph-admin-user-playbook.yml \
+                     -e tripleo_admin_user=ceph-admin \
+                     -e distribute_private_key=true \
+                     --limit $CEPHADM_PUBLIC_PRIVATE_SSH_LIST
 
+    ansible-playbook -i $INV \
+                     $PLAYBOOKS/ceph-admin-user-playbook.yml \
+                     -e tripleo_admin_user=ceph-admin \
+                     -e distribute_private_key=false \
+                     --limit $CEPHADM_PUBLIC_SSH_LIST
+fi
 
-# PLAY: bootstrap node
-# BOOTSTRAP
-# - name: Bootstrap Ceph
-#   import_role:
-#     name: tripleo_cephadm
-#     tasks_from: bootstrap
+# DEPLOY CEPH
+ansible-playbook -i $INV \
+                 -v \
+                 $PLAYBOOKS/cli-deployed-ceph.yaml \
+                 -e baremetal_deployed_path="$PWD/deployed-metal-other-3.yaml" \
+                 -e new_ceph_spec_path="$PWD/generated_ceph_spec.yaml" 
 
-# APPLY SPEC
-# - name: Apply Ceph spec
-#   import_role:
-#     name: tripleo_cephadm
-#     tasks_from: apply_spec
-
-# customer crush rules should be set manually via cephadm
+# Custom crush rules should be set manually via cephadm
