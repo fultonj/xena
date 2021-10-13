3bd9ba188d409c50846a43fedc53d9adc0445217
John Fulton <fulton@redhat.com>
2021-10-13 12:40:03 +0000

    one does not simply use double brackets

--- standalone/ceph.sh
+++ standalone/ceph.sh
@@ -1,24 +1,18 @@
 #!/usr/bin/env bash
 
-TEST=0
+SPEC=0
 PLAY=1
 CLI=0
 
-if [[ $TEST -eq 1 ]]; then
-    # confirm localhost inventory works
-    ansible -i fake_workdir/tripleo-ansible-inventory.yaml -m ping all
-    # confirm that ansible module can parse provided inputs
+if [ $SPEC -eq 1 ]; then
     ansible localhost -m ceph_spec_bootstrap \
       -a "deployed_metalsmith=fake_workdir/deployed_metal.yaml \
+          new_ceph_spec=fake_workdir/ceph_spec.yaml
           tripleo_roles=/usr/share/openstack-tripleo-heat-templates/roles/Standalone.yaml"
-
-    ls -l ~/ceph_spec.yaml 
-    cat ~/ceph_spec.yaml
-    rm -v ~/ceph_spec.yaml
 fi
 
 
-if [[ $PLAY -eq 1 ]]; then
+if [ $PLAY -eq 1 ]; then
     PLAYBOOKS="/usr/share/ansible/tripleo-playbooks"
     THT="/usr/share/openstack-tripleo-heat-templates"
     INV="$PWD/fake_workdir/tripleo-ansible-inventory.yaml"
@@ -26,19 +20,20 @@ if [[ $PLAY -eq 1 ]]; then
            -v \
            $PLAYBOOKS/cli-deployed-ceph.yaml \
            -e baremetal_deployed_path="$PWD/fake_workdir/deployed_metal.yaml" \
-           -e osd_spec_path="$PWD/fake_workdir/osd_spec.yaml" \
+           -e ceph_spec_path="$PWD/fake_workdir/ceph_spec.yaml" \
            -e deployed_ceph_tht_path="$PWD/deployed_ceph.yaml" \
            -e tripleo_roles_path="$THT/roles/Standalone.yaml" \
            -e tripleo_cephadm_container_image="daemon" \
            -e tripleo_cephadm_container_ns="quay.ceph.io/ceph-ci" \
            -e tripleo_cephadm_container_tag="latest-pacific-devel" \
            -e working_dir="$PWD/fake_workdir" \
+           -e dynamic_ceph_spec="false" \
            -e storage_network_name="ctlplane" \
            -e storage_mgmt_network_name="ctlplane"
 fi
 
 
-if [[ $CLI -eq 1 ]]; then
+if [ $CLI -eq 1 ]; then
     openstack overcloud ceph deploy \
           fake_workdir/deployed_metal.yaml \
           --working-dir fake_workdir \
