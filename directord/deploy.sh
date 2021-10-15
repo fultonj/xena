25a26dbfa9f829a39bbafee904317bc135d2bf5e
John Fulton <fulton@redhat.com>
2021-10-15 20:23:55 +0000

    add handy shortcuts

--- directord/deploy.sh
+++ directord/deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Clean cache on all nodes
+# directord exec --verb CACHEEVICT all
+
+# Purge jobs from server
+# directord manage --purge-jobs
+
+
 pushd ~/task-core/examples/directord/services
 
 task-core -s . -i ~/task-core-inventory-hackfest.yaml -r ../basic/2node_roles.yaml -d
