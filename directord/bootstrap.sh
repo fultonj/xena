853aed430599873e3d3478a3cc22e2e6f0d3eed9
John Fulton <fulton@redhat.com>
2021-10-26 17:49:12 +0000

    switch to zmq-encryption-catalog
    https://github.com/directord/directord/commit/1e80fbf0f4b52a5ff6c46da8d6feb5115e0a02a1

--- directord/bootstrap.sh
+++ directord/bootstrap.sh
@@ -5,7 +5,7 @@ pushd ~
 ./directord/.tox/venv/bin/directord bootstrap \
                                     --catalog directord-inventory-catalog.yaml \
                                     --catalog \
-                                    directord/tools/directord-dev-bootstrap-catalog.yaml \
+                                    directord/tools/directord-dev-bootstrap-zmq-encryption-catalog.yaml \
                                     --key-file ~/.ssh/id_ed25519
 
 popd
