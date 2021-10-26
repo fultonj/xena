f3cdd17f15b92704b8833bfbdac315f8191aa818
John Fulton <fulton@redhat.com>
2021-10-26 17:00:43 +0000

    use directord not mwhahaha as source for task-core

--- directord/task-core/install.sh
+++ directord/task-core/install.sh
@@ -5,7 +5,7 @@ sudo dnf -y install cmake make gcc gcc-c++ openssl-devel
 sudo dnf -y update libarchive
 
 if [[ ! -d ~/task-core ]]; then
-    git clone https://github.com/mwhahaha/task-core
+    git clone https://github.com/directord/task-core
 fi
 
 sudo /opt/directord/bin/pip3 install task-core/
