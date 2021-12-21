444877e205466f6a351d44693cef46bdf4ee6557
John Fulton <fulton@redhat.com>
2021-12-21 02:49:35 +0000

    Update standalone ceph.sh for deployed ceph
    - Add USER feature which exercises recent patch*
    - No longer need a playbook option as client works without auth*
    - Have KILL clean disks
    - Update to v6.0.6-stable-6.0-pacific-centos-8-x86_64 container
    
    * https://review.opendev.org/q/topic:%22ceph_user_disable%22

--- standalone/ceph.sh
+++ standalone/ceph.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SPEC=0
-PLAY=0
+USER=1
 CLI=1
 KILL=0
 
@@ -12,41 +12,35 @@ if [ $SPEC -eq 1 ]; then
           tripleo_roles=/usr/share/openstack-tripleo-heat-templates/roles/Standalone.yaml"
 fi
 
-
-if [ $PLAY -eq 1 ]; then
-    PLAYBOOKS="/usr/share/ansible/tripleo-playbooks"
-    THT="/usr/share/openstack-tripleo-heat-templates"
-    INV="$PWD/fake_workdir/tripleo-ansible-inventory.yaml"
-    ansible-playbook -i $INV \
-           -v \
-           $PLAYBOOKS/cli-deployed-ceph.yaml \
-           -e baremetal_deployed_path="$PWD/fake_workdir/deployed_metal.yaml" \
-           -e ceph_spec_path="$PWD/fake_workdir/ceph_spec.yaml" \
-           -e deployed_ceph_tht_path="$PWD/deployed_ceph.yaml" \
-           -e tripleo_roles_path="$THT/roles/Standalone.yaml" \
-           -e tripleo_cephadm_container_image="daemon" \
-           -e tripleo_cephadm_container_ns="quay.ceph.io/ceph-ci" \
-           -e tripleo_cephadm_container_tag="latest-pacific-devel" \
-           -e working_dir="$PWD/fake_workdir" \
-           -e dynamic_ceph_spec="false"
+if [ $USER -eq 1 ]; then
+    sudo openstack overcloud ceph user enable \
+              fake_workdir/ceph_spec.yaml \
+              --working-dir fake_workdir \
+              --stack standalone
 fi
 
-
 if [ $CLI -eq 1 ]; then
-    openstack overcloud ceph deploy \
+    sudo openstack overcloud ceph deploy \
           fake_workdir/deployed_metal.yaml \
           --working-dir fake_workdir \
           --network-data fake_workdir/network_data.yaml \
           --roles-data /usr/share/openstack-tripleo-heat-templates/roles/Standalone.yaml \
           --ceph-spec fake_workdir/ceph_spec.yaml \
+          --skip-user-create \
           --container-namespace quay.io/ceph \
           --container-image daemon \
-          --container-tag v6.0.4-stable-6.0-pacific-centos-8-x86_64 \
+          --container-tag v6.0.6-stable-6.0-pacific-centos-8-x86_64 \
           --stack standalone \
           -y -o deployed_ceph.yaml
 fi
 
 if [ $KILL -eq 1 ]; then
     FSID=$(sudo cephadm ls --no-detail | jq .[].fsid | head -1 | sed s/\"//g)
+    sudo systemctl stop ceph-osd@*
+    sudo /usr/sbin/cephadm zap-osds --force --fsid $FSID
     sudo /usr/sbin/cephadm rm-cluster --force --fsid $FSID
+    sudo lvremove /dev/vg2/data-lv2 --yes
+    sudo vgremove /dev/vg2 --yes
+    bash disks.sh
+    lsblk
 fi
