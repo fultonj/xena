d6e890cfc81c3b3614cf73cf1dd803f7581eaaa2
John Fulton <fulton@redhat.com>
2022-02-02 16:16:46 +0000

    EnableSysctlPingGroupRange
    Follow up to 109b5abd4eabb702cc2909c8d6584c305cc75978

--- standalone/pre.sh
+++ standalone/pre.sh
@@ -68,7 +68,8 @@ fi
 
 if [[ $DNS -eq 1 ]]; then
     GW=192.168.122.1
-    sudo ping -c 1 $GW > /dev/null
+    sudo sysctl -w net.ipv4.ping_group_range="0 1000"
+    ping -c 1 $GW > /dev/null
     if [[ $? -ne 0 ]]; then
         echo "Cannot ping $GW. Aborting."
         exit 1
