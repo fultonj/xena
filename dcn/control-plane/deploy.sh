f323fb0141b233c78b15f38b4bd1ecb4eb506160
John Fulton <fulton@redhat.com>
2022-03-25 17:33:04 -0400

    have control-plane deploy use metal and ceph scripts

--- dcn/control-plane/deploy.sh
+++ dcn/control-plane/deploy.sh
@@ -1,106 +1,55 @@
 #!/bin/bash
 
-IRONIC=0
-CEPH=0
-HEAT=1
-
 STACK=control-plane
-DIR=~/config-download
 
 source ~/stackrc
-# -------------------------------------------------------
-METAL="../../metalsmith/deployed-metal-${STACK}.yaml"
-NET="../../metalsmith/deployed-network-${STACK}.yaml"
-VIP="../../metalsmith/deployed-vips-${STACK}.yaml"
-if [[ $IRONIC -eq 1 ]]; then
-    if [[ ! -e $METAL ]]; then
-        echo "$METAL is missing. Deploying nodes with metalsmith."
-        pushd ../../metalsmith
-        bash provision.sh $STACK
-        popd
-    fi
-    if [[ ! -e $METAL ]]; then
-        echo "$METAL is missing after deployment attempt. Going to retry once."
-        pushd ../../metalsmith
-        bash undeploy_failures.sh
-        bash provision.sh $STACK
-        popd
-        if [[ ! -e $METAL ]]; then
-            echo "$METAL is still missing. Aborting."
-            exit 1
-        fi
-    fi
-    echo "Finished with baremetal"
-fi
-if [[ ! -e deployed-metal-$STACK.yaml && $NEW_SPEC -eq 0 ]]; then
-    cp $METAL deployed-metal-$STACK.yaml
-    cp $NET deployed-network-$STACK.yaml
-    cp $VIP deployed-vips-$STACK.yaml
-fi
-# -------------------------------------------------------
-if [[ $CEPH -eq 1 ]]; then
-    openstack overcloud ceph deploy \
-              $PWD/deployed-metal-$STACK.yaml \
-              -y -o $PWD/deployed-ceph-$STACK.yaml \
-              --container-image-prepare ~/containers-prepare-parameter.yaml \
-              --network-data ~/oc0-network-data.yaml \
-              --roles-data control_plane_roles.yaml \
-              --stack $STACK
-fi
-# -------------------------------------------------------
-if [[ $HEAT -eq 1 ]]; then
-    if [[ ! -d ~/templates ]]; then
-        ln -s /usr/share/openstack-tripleo-heat-templates ~/templates
-    fi
-    if [[ $NODE_COUNT -gt 0 ]]; then
-        FOUND_COUNT=$(metalsmith -f value -c "Hostname" list | wc -l)
-        if [[ $NODE_COUNT != $FOUND_COUNT ]]; then
-            echo "Expecting $NODE_COUNT nodes but $FOUND_COUNT nodes have been deployed"
-            exit 1
-        fi
-    fi
-    if [[ ! -e deployed-ceph-$STACK.yaml ]]; then
-        echo "deployed-ceph-$STACK.yaml is missing, why didn't ceph.sh make it?"
-        exit 1
-    fi
 
-    HEAT_POD=quay.io/tripleomaster/openstack-heat-all:current-tripleo
-    podman pull $HEAT_POD
-    echo "Runing openstack overcloud deploy"
+if [[ ! -d ~/templates ]]; then
+    ln -s /usr/share/openstack-tripleo-heat-templates ~/templates
+fi
 
-    # Use this as needed to speed up stack updates
-    # --disable-container-prepare \
+if [[ ! -e deployed-ceph-$STACK.yaml ]]; then
+    echo "deployed-ceph-$STACK.yaml is missing, why didn't ceph.sh make it?"
+    exit 1
+fi
+
+HEAT_POD=quay.io/tripleomaster/openstack-heat-all:current-tripleo
+podman pull $HEAT_POD
+echo "Runing openstack overcloud deploy"
+
+# Use this as needed to speed up stack updates
+# --disable-container-prepare \
     
-    time openstack overcloud deploy \
-         --templates ~/templates \
-         --stack $STACK \
-         --timeout 90 \
-         --heat-type pod --skip-heat-pull \
-         --heat-container-engine-image $HEAT_POD \
-         --heat-container-api-image $HEAT_POD \
-         -e ~/templates/environments/network-environment.yaml \
-         -e ~/templates/environments/low-memory-usage.yaml \
-         -e ~/templates/environments/podman.yaml \
-         -e ~/templates/environments/docker-ha.yaml \
-         -e ~/templates/environments/cephadm/cephadm.yaml \
-         -r control_plane_roles.yaml \
-         -n ~/oc0-network-data.yaml \
-         -e ~/containers-prepare-parameter.yaml \
-         -e ~/re-generated-container-prepare.yaml \
-         -e ~/oc0-domain.yaml \
-         -e ~/overcloud-0-yml/nova-tpm.yaml \
-         -e ~/overcloud-0-yml/network-env.yaml \
-         -e ~/xena/env_common/overrides.yaml \
-         -e deployed-vips-$STACK.yaml \
-         -e deployed-network-$STACK.yaml \
-         -e deployed-metal-$STACK.yaml \
-         -e deployed-ceph-$STACK.yaml \
-         -e overrides.yaml \
-         --libvirt-type qemu
-         # ONE
-         # TWO
+time openstack overcloud deploy \
+     --templates ~/templates \
+     --stack $STACK \
+     --timeout 90 \
+     --heat-type pod --skip-heat-pull \
+     --heat-container-engine-image $HEAT_POD \
+     --heat-container-api-image $HEAT_POD \
+     -e ~/templates/environments/network-environment.yaml \
+     -e ~/templates/environments/low-memory-usage.yaml \
+     -e ~/templates/environments/podman.yaml \
+     -e ~/templates/environments/docker-ha.yaml \
+     -e ~/templates/environments/cephadm/cephadm.yaml \
+     -r control_plane_roles.yaml \
+     -n ~/oc0-network-data.yaml \
+     -e ~/containers-prepare-parameter.yaml \
+     -e ~/re-generated-container-prepare.yaml \
+     -e ~/oc0-domain.yaml \
+     -e ~/overcloud-0-yml/nova-tpm.yaml \
+     -e ~/overcloud-0-yml/network-env.yaml \
+     -e ~/xena/env_common/overrides.yaml \
+     -e deployed-vips-$STACK.yaml \
+     -e deployed-network-$STACK.yaml \
+     -e deployed-metal-$STACK.yaml \
+     -e deployed-ceph-$STACK.yaml \
+     -e overrides.yaml \
+     --libvirt-type qemu
+     # ONE
+     # TWO
 
-    # For stack updates when central dcn will use dcn{0,1} ceph clusters
-    # -e glance_update.yaml \
-    # -e ../ceph-export-2-stacks.yaml \
-fi
+# For stack updates when central dcn will use dcn{0,1} ceph clusters
+# -e glance_update.yaml \
+# -e ../ceph-export-2-stacks.yaml \
+
