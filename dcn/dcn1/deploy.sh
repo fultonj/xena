773ff898c9015446502010e14e5ff8b10abe6ae8
John Fulton <fulton@redhat.com>
2022-03-31 18:13:23 -0400

    dcn1 deploy should not overwrite itself

--- dcn/dcn1/deploy.sh
+++ dcn/dcn1/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export STACK=dcn1
 
-cp -v -f ../dcn0/deploy.sh .
-sed s/dcn0/dcn1/g -i dcn1/deploy.sh
+cp -v -f ../dcn0/deploy.sh ${STACK}_deploy.sh
+sed s/dcn0/dcn1/g -i ${STACK}_deploy.sh
 
 cp -v -f ../dcn0/glance.yaml .
 sed s/dcn0/dcn1/g -i glance.yaml
@@ -10,4 +10,4 @@ sed s/dcn0/dcn1/g -i glance.yaml
 cp -v -f ../dcn0/overrides.yaml .
 sed s/dcn0/dcn1/g -i overrides.yaml
 
-bash deploy.sh
+bash ${STACK}_deploy.sh
