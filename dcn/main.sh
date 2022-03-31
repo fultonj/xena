3c88f7160f1aff519d3dbfde04926ba18053ded3
John Fulton <fulton@redhat.com>
2022-03-31 18:38:01 -0400

    add dcn1 and central update to main

--- dcn/main.sh
+++ dcn/main.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 NEW_CLIENT=0
-CENTRAL_DEPLOY=0
-CENTRAL_SANITY=0
-CENTRAL_EXPORT=0
+CENTRAL_DEPLOY=1
+CENTRAL_SANITY=1
+CENTRAL_EXPORT=1
 DCN0_DEPLOY=1
-DCN1_DEPLOY=0
-DCN_EXPORT=0
+DCN1_DEPLOY=1
+CENTRAL_UP=1
 
 source ~/stackrc
 
@@ -70,4 +70,40 @@ if [[ $DCN0_DEPLOY -eq 1 ]]; then
     pushd $STACK
     bash deploy.sh
     popd
+
+    source ~/control-planerc
+    openstack aggregate show dcn0
+    if [[ $? -gt 0 ]]; then
+        echo "openstack aggregate dcn0 was not created. Aborting."
+        exit 1
+    fi
+    source ~/stackrc
+fi
+
+if [[ $DCN1_DEPLOY -eq 1 ]]; then
+    STACK=dcn1
+    bash metal.sh $STACK
+    bash ceph.sh $STACK
+    pushd $STACK
+    bash deploy.sh
+    popd
+fi
+
+if [[ $CENTRAL_UP -eq 1 ]]; then
+    openstack overcloud export ceph -f --stack dcn0,dcn1
+    if [[ ! -e ceph-export-2-stacks.yaml ]]; then
+        echo "Failure: openstack overcloud export ceph --stack dcn0,dcn1"
+        exit 1
+    fi
+    pushd control-plane
+    if [[ -e deploy-update.sh ]]; then
+        rm -f deploy-update.sh
+    fi
+    cp deploy.sh deploy-update.sh
+    sed -i s/qemu/qemu\ \\\\/g deploy-update.sh
+    sed -i s/#\ ONE/\\-e\ glance_update.yaml\ \\\\/g deploy-update.sh
+    sed -i s/#\ TWO/\\-e\ \\.\\.\\/ceph-export-2-stacks.yaml/g deploy-update.sh
+    bash deploy-update.sh
+    popd
+    echo "You may now test the deployment with validations/use-multistore-glance.sh"
 fi
