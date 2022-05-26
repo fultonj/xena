80f046b2ddb2eb5869a896f60d4d852916196d47
John Fulton <fulton@redhat.com>
2022-05-26 20:27:10 +0000

    add hci undeploy.sh

--- hci/undeploy.sh
+++ hci/undeploy.sh
@@ -2,22 +2,29 @@
 
 CLEAN=1
 STACK=hci
+METAL=deployed-metal-$STACK.yaml
+TMP=/tmp/ceph_nodes_$STACK
+grep computehci $METAL \
+    | grep -v CephStorageHostnameFormat \
+    | grep -v index \
+    | awk {'print $2'} > $TMP
 
 openstack overcloud delete $STACK --yes
 
 pushd ../metalsmith
 bash unprovision.sh $STACK
-rm -f deployed-metal-$STACK.yaml
+rm -f deployed-{metal,network}-$STACK.yaml
 popd
 
-for F in deployed-metal-$STACK.yaml cirros-0.4.0-x86_64-disk.{raw,img} tempest-deployer-input.conf; do
+for F in generated_ceph_spec.yaml generated_deployed_ceph.yaml deployed-{metal,network}-$STACK.yaml cirros-0.4.0-x86_64-disk.{raw,img} tempest-deployer-input.conf; do
     if [[ -e $F ]]; then
         rm -f $F
     fi
 done
 
 if [[ $CLEAN -eq 1 ]]; then
-    for I in $(seq 0 2); do
-        bash ../metalsmith/clean-disks.sh oc0-ceph-$I
+    for H in $(cat $TMP); do
+        bash ../metalsmith/clean-disks.sh $H
     done
 fi
+rm -f $TMP
