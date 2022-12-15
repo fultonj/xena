efc2cdecf61b4c53b9e43462228939b4e2decd3c
alfrgarc <alfrgarc@redhat.com>
2022-12-15 12:40:39 -0500

    Updated the paths in kill.sh and metal.sh to the paths used in the downstream CI

--- ir/undercloud/kill.sh
+++ ir/undercloud/kill.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [[ ! -e ~/virt/network/baremetal_deployment.yaml ]]; then
+if [[ ! -e /home/stack/composable_roles/network/baremetal_deployment.yaml ]]; then
     echo "baremetal_deployment.yaml is missing"
     exit 1
 fi
 
 openstack overcloud delete overcloud --yes
 
-openstack overcloud node unprovision --all -y --stack overcloud ~/virt/network/baremetal_deployment.yaml
+openstack overcloud node unprovision --all -y --stack overcloud /home/stack/composable_roles/network/baremetal_deployment.yaml
